49f38a7c4fd010f867cdcefd2837e4f1c3b1040a
nero-bc <126649764+nero-bc@users.noreply.github.com>
2024-07-22 19:53:44 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -211,13 +211,13 @@ async def cancel_command_handler(c: Client, m: Message):
         await delete_all(root=f"downloads/{user_id}/")
         queueDB.update({user_id: {"videos": [], "subtitles": [], "audios": []}})
         formatDB.update({user_id: None})
-        await message.reply_text(
+        await m.reply_text(
             "Successfully cancelled the operation.",
             quote=True
         )
     except Exception as e:
         LOGGER.error(f"Error during cancel operation for user {user_id}: {e}")
-        await message.reply_text(
+        await m.reply_text(
             "An error occurred while cancelling the operation. Please try again later.",
             quote=True
         )
